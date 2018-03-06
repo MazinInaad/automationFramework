e7f30df87a5964c69df9f85cc6c7244ac4b48e5d
Dev_InaadM <mazin.inaad@capgemini.com>
2018-03-06 09:31:42 +0100

    OurAssertions exception handling bug fixed

--- src/test/java/com/capgemini/resources/OurAssertions.java
+++ src/test/java/com/capgemini/resources/OurAssertions.java
@@ -1,10 +1,10 @@
 package com.capgemini.resources;
 
 
-import junit.framework.AssertionFailedError;
+import com.capgemini.ourWebdriver.OurWebDriver;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -30,7 +30,7 @@ public class OurAssertions {
     public static void assertEquals(String expected, String actual) {
         try {
             Assert.assertEquals(expected, actual);
-        } catch (AssertionFailedError e) {
+        } catch (AssertionError e) {
             String failLocation = getTrace(e.getStackTrace());
             failedAssertions.add(getFailHeader(failLocation) + "Expected: " + expected + "\nActual: " + actual);
             OurScenario.takeScreenShot("Assertion_failed_" + failedAssertions.size());
@@ -50,7 +50,7 @@ public class OurAssertions {
     public static void assertEquals(int expected, int actual) {
         try {
             Assert.assertEquals(expected, actual);
-        } catch (AssertionFailedError e) {
+        } catch (AssertionError e) {
             String failLocation = getTrace(e.getStackTrace());
             failedAssertions.add(getFailHeader(failLocation) + "Expected int: " + expected + "\nActual int: " + actual);
             OurScenario.takeScreenShot("Assertion_failed_" + failedAssertions.size());
@@ -70,7 +70,7 @@ public class OurAssertions {
     public static void assertContains(String mainString, String containsString) {
         try {
             Assert.assertTrue(mainString.contains(containsString));
-        } catch (AssertionFailedError e) {
+        } catch (AssertionError e) {
             String failLocation = getTrace(e.getStackTrace());
             failedAssertions.add(getFailHeader(failLocation) + "String: " + mainString + "\ndoes not contain: " + containsString);
             OurScenario.takeScreenShot("Assertion_failed_" + failedAssertions.size());
@@ -90,9 +90,46 @@ public class OurAssertions {
     public static void assertEndsWith(String mainString, String endsWithString) {
         try {
             Assert.assertTrue(mainString.endsWith(endsWithString));
-        } catch (AssertionFailedError e) {
+        } catch (AssertionError e) {
             String failLocation = getTrace(e.getStackTrace());
             failedAssertions.add(getFailHeader(failLocation) + "String: " + mainString + "\ndoes not end with " + endsWithString);
+            OurScenario.takeScreenShot("Assertion_failed_" + failedAssertions.size());
+            assertionFailed = true;
+        }
+    }
+
+    /**
+     * Custom Assert function which tries the assertTrue method from {@link Assert}.
+     * If this fails, the fail location is registered (so that it can be printed later)
+     * and a screenshot is taken with name 'Assertion_failed_id'.
+     *
+     * @param mainString The String to be tested
+     * @param startsWithString The substring which the test string should end with
+     * @see Assert
+     */
+    public static void assertStartsWith(String mainString, String startsWithString) {
+        try {
+            Assert.assertTrue(mainString.startsWith(startsWithString));
+        } catch (AssertionError e) {
+            String failLocation = getTrace(e.getStackTrace());
+            failedAssertions.add(getFailHeader(failLocation) + "String: " + mainString + "\ndoes not start with " + startsWithString);
+            OurScenario.takeScreenShot("Assertion_failed_" + failedAssertions.size());
+            assertionFailed = true;
+        }
+    }
+
+    /**
+     * Custom Assert function which verifies that the passed element (By by) is present in the passed OurWebdriver
+     * @param browser The driver with which to search for the element
+     * @param by The selector for the element
+     */
+    public static void verifyElementpresent(OurWebDriver browser, By by){
+        try{
+            Assert.assertTrue(browser.findElements(by).size() > 0);
+        }
+        catch (AssertionError e){
+            String failLocation = getTrace(e.getStackTrace());
+            failedAssertions.add(getFailHeader(failLocation) + "Element: " + by.toString() + "\ndoes not exist.");
             OurScenario.takeScreenShot("Assertion_failed_" + failedAssertions.size());
             assertionFailed = true;
         }
