609b7384263e1538ed5e1c67c94500235589a3ac
Dev_InaadM <mazin.inaad@capgemini.com>
2018-03-27 19:59:07 +0200

    Added new reporting

--- src/test/java/com/capgemini/AppTest.java
+++ src/test/java/com/capgemini/AppTest.java
@@ -1,15 +1,27 @@
 package com.capgemini;
 
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(jsonReport = "target/cucumber.json", jsonUsageReport = "target/cucumber-usage.json", outputFolder = "target/", detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = false, reportPrefix = "DNA_results_")
+@CucumberOptions(
+        plugin = {"pretty:target/cucumber-pretty.txt", "json:target/cucumber.json", "usage:target/cucumber-usage.json"},
+        features = "src/test/java/com/project/features/",
+        glue = {"com/project/steps/", "com/capgemini/ourWebdriver/"}
+)
+
+public class AppTest extends TestCase {
     /**
      * Create the test case
      *
